bce150fa9dafe9b03b37feca3240ab29712d26e5
Akash Chaudhari <akashchaudhari1013@gmail.com>
2025-07-28 19:59:30 +0530

    host allowed

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-863074gf)ec63zcf93=v3kej7eyhfr7gfds3ylg!(l1sp!^p14
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
