8e5e0cbc8b57775a493d1b19c556523380d69aa8
Akash Chaudhari <akashchaudhari1013@gmail.com>
2025-08-03 16:11:20 +0530

    h

--- api/models.py
+++ api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    html_file = models.FileField(upload_to='templates/uploads/html/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Item
 
 class ItemSerializer(serializers.ModelSerializer):
+    html_file = serializers.FileField(required=False, allow_null=True)
+    
     class Meta:
         model = Item
         fields = '__all__'
+    
+    def validate_html_file(self, value):
+        if value is not None:
+            if not value.name.endswith('.html'):
+                raise serializers.ValidationError("Only .html files are allowed.")
+        return value
